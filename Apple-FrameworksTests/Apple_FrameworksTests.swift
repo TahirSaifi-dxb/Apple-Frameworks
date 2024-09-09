588bed06af46a60f813add2eb053ef9144339a8f
Tahir Saifi <tahirhakim.52@me.com>
2024-09-09 17:19:45 +0400

    Initial Commit

---
+++ Apple-FrameworksTests/Apple_FrameworksTests.swift
@@ -0,0 +1,16 @@
+//
+//  Apple_FrameworksTests.swift
+//  Apple-FrameworksTests
+//
+//  Created by Tahir Saifi on 09/09/2024.
+//
+
+import Testing
+
+struct Apple_FrameworksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
